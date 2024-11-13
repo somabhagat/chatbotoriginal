2030630ede69f4a5233e7bf0130eacfdcc50288a
somabhagat <bhagatsoma@gmail.com>
2024-11-13 15:36:20 +0100

    Learnen wie Gitlab nutzen soll mit React grundlagen übung

---
+++ misc/lernen/js-basics/index.js
@@ -0,0 +1,13 @@
+//performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name+' '+lastName);
+} 
+
+//calculate a value
+function square(number) {
+    return number * number;
+}
+square(2);
+let result = square(2);
+console.log(square(2));
+greet('John', 'Doe',);
