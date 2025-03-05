1dbb6a99e54bc372a3bd9dacde63a2e0c568ab03
Soma <88511904+somabhagat@users.noreply.github.com>
2025-03-05 11:08:46 +0100

    Add files via upload

---
+++ mylocalbots/sb1/babel.config.js
@@ -0,0 +1,31 @@
+/*
+ * This babel configuration is used along with Jest for execute tests,
+ * do not modify to avoid conflicts with webpack.config.js.
+ */
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',  // preset wort hinzufügen
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-flow',  // Diese Zeile hinzufügen
+    '@babel/preset-typescript',  // Diese Zeile hinzufügen
+  ],
+  plugins: [
+    require('@babel/plugin-transform-modules-commonjs'),
+    require('@babel/plugin-transform-runtime'),
+  ],
+}
